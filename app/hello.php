7916a0d500e3fd2500635ee5181027baad0cafed
amonrg <amontesrg@gmail.com>
2026-08-11 19:42:51 -0600

    Add app service

---
+++ app/hello.php
@@ -0,0 +1,19 @@
+<?php
+    $env = parse_ini_file('.env');
+    $password = $env["POSTGRES_PASSWORD"];
+    $username = $env["POSTGRES_USERNAME"];
+    $db = $env["POSTGRES_DB"];
+    $host = $env["POSTGRES_HOST"]
+    $dbconn = pg_connect("host=" . $host . "dbname=" . $db . "user=" . $username . "password=" . $password) or die('Could not connect: ' . pg_last_error());
+
+    echo "Inventory System" . "<br>";
+
+    if ($dbconn) {
+        $version = pg_version($dbconn);
+
+        echo "Database connection: OK" . "<br>";
+        echo "Database response: " . $version['client'] . "<br>";
+    }
+
+    pg_close($dbconn)
+?>
