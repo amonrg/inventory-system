637d5f3800136e3be71e6886c69f572ae0def8d0
amonrg <amontesrg@gmail.com>
2026-08-20 20:39:05 -0600

    Change env variables reading method to the compose file

--- app/hello.php
+++ app/hello.php
@@ -1,10 +1,13 @@
 <?php
-    $env = parse_ini_file('.env');
-    $password = $env["POSTGRES_PASSWORD"];
-    $username = $env["POSTGRES_USERNAME"];
-    $db = $env["POSTGRES_DB"];
-    $host = $env["POSTGRES_HOST"];
-    $dbconn = pg_connect("host=" . $host . " dbname=" . $db . " user=" . $username . " password=" . $password) or die('Could not connect: ' . pg_last_error());
+    $password = getenv('POSTGRES_PASSWORD');
+    $username = getenv('POSTGRES_USERNAME');
+    $db = getenv('POSTGRES_DB');
+    $host = getenv('POSTGRES_HOST');
+    $dbconn = pg_connect("host=" . $host . 
+                         " dbname=" . $db .
+                         " user=" . $username . 
+                         " password=" . $password) 
+                         or die('Could not connect: ' . pg_last_error());
 
     echo "Inventory System" . "<br>";
 
